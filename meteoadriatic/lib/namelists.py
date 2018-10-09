f2499d04cd78a4400ec8ef7c558a350e36fb9314
meteoadriatic <toman@meteoadriatic.net>
2018-10-09 14:06:55 +0200

    Code cleanup, adding comments

--- meteoadriatic/lib/namelists.py
+++ meteoadriatic/lib/namelists.py
@@ -1,6 +1,5 @@
 import sys
 import fileinput
-from shutil import copyfile
 from datetime import timedelta
 
 
@@ -16,18 +15,22 @@ def calculate_nests_date(domains_definition, wrf_init_time):
     return nests_start, nests_end
 
 
-def create_namelist_wps(namelist_wps_static, namelist_wps_dynamic, start_date, end_date, max_dom, interval_seconds):
-    copyfile(namelist_wps_static, namelist_wps_dynamic)
+def create_namelist_wps(namelist_wps_dynamic, start_date, end_date, max_dom, interval_seconds):
     for line in fileinput.input([namelist_wps_dynamic], inplace=True):
+
         if line.strip().startswith('max_dom = '):
             line = " max_dom = " + str(max_dom) + ",\n"
+
         if line.strip().startswith('start_date = '):
             insert_string = "', ".join(start_date)
             line = " start_date = '" + insert_string + "'" + ",\n"
+
         if line.strip().startswith('end_date = '):
             insert_string = "', ".join(end_date)
             line = " end_date = '" + insert_string + "'" + ",\n"
+
         if line.strip().startswith('interval_seconds = '):
             insert_string = "', ".join(end_date)
             line = " interval_seconds = " + str(interval_seconds) + "\n"
+
         sys.stdout.write(line)
--- meteoadriatic/run_wps.py
+++ meteoadriatic/run_wps.py
@@ -1,39 +1,83 @@
 from lib.time_calc import wrf_init_time
-from lib.create_namelists import calculate_nests_date, create_namelist_wps
+from lib import namelists
 from date_info import *
+from shutil import copyfile
 
-namelist_wps_static = "../runs/test_domain/static/namelist.wps"
-namelist_wps_dynamic = "../runs/test_domain/wpsprd/namelist.wps"
-
-'''
-This file prepares prerequisites required to run WPS suite (ungrib.exe and metgrid.exe) and then executes it.
-
-Step 1. Use namelist_wps_static template to create dynamic version with correct start and end datetime.
-We need to edit "start_date = ..." and "end_date = ..." lines
+namelist_wps_static = "runs/test_domain/static/namelist.wps"
+namelist_wps_dynamic = "runs/test_domain/wpsprd/namelist.wps"
 
 '''
+This module prepares prerequisites required to run WPS suite (ungrib.exe and metgrid.exe) and then executes it.
 
+Requirements: acquired input (LBC) files in runs/<domainname>/input
+Module output: prepared metgrid_* files in runs/<domainname>/wpsprd 
+'''
+
+
+'''
+----------------------------------------
+--------------- Step 1. ----------------
+------------- namelist.wps -------------
+----------------------------------------
+ 
+Use namelist_wps_static template to create dynamic version with correct start and end datetime.
+We take namelist.wps template from domain static directory and according to particular run variables, edit lines:
+
+"start_date = ..."         (nested)
+"end_date = ..."           (nested)
+"max_dom = ..."            (single)
+"interval_seconds = ..."   (single)
+
+'''
+
+
+'''
+Determine domains definition from config/user input. We need to know number of configured domains
+For each of them we need to get start delay time (hours after MOAD) and run length. Information about
+nests should be given in form of list with each entry in format 'x:y:z' where x is nest ID, y is
+start delay and z is run length for that nest. MOAD (mother of all domains) always has definition '0:0:0'
+and is not included in user configuration entries, but added in domains definition in code below.  
+'''
 nests_definition = nests.split(",")
 domains_definition = ['0:0:0'] + nests_definition
 max_dom = len(domains_definition)
 
-start_date, end_date = calculate_nests_date(domains_definition, wrf_init_time)
+
+'''
+Here we calculate start_date and end_date and interval_seconds 
+namelist.wps entries for all requested domains. Proper format handling is also done here. 
+'''
+start_date, end_date = namelists.calculate_nests_date(domains_definition, wrf_init_time)
 start_date = [dt.strftime("%Y-%m-%d_%H:%M:%S") for dt in start_date]
 end_date = [dt.strftime("%Y-%m-%d_%H:%M:%S") for dt in end_date]
 
 interval_seconds = int(lbc_frequency) * 3600
 
-create_namelist_wps(namelist_wps_static, namelist_wps_dynamic, start_date, end_date, max_dom, interval_seconds)
-
+copyfile(namelist_wps_static, namelist_wps_dynamic)
+namelists.create_namelist_wps(namelist_wps_dynamic, start_date, end_date, max_dom, interval_seconds)
 
 
 '''
-Mistery:
+?????????????????????????????????????????????????????????????????????
+Mystery:
 In this file, relative link to runs/test_domain/static is
 "../runs/test_domain"
 however, in download_strc_py, which is at the same level, it is 
 "runs/test_domain"
 
 What's going on? A Pycharm joke?
+?????????????????????????????????????????????????????????????????????
+'''
+
+
+'''
+This file prepares prerequisites required to run WPS suite (ungrib.exe and metgrid.exe) and then executes it.
+
+----------------------------------------
+--------------- Step 2. ----------------
+--------------- linking ----------------
+----------------------------------------
+
+All tables and executables has to be linked into WPS process directory.
 
 '''
